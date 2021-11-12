4d8ce808efffa4ea08112fd7a13d0c8e6e7e6de5
Mark Drummond <mjamesd@gmail.com>
2021-11-11 18:10:37 -0800

    Removed unused dirs. Added chalk. Began init()

--- index.js
+++ index.js
@@ -1,13 +1,54 @@
-// TODO: Include packages needed for this application
+// Use Inquirer package
+const inquirer = require('./node_modules/inquirer');
+// Use Chalk package to color console.log
+// Usage: console.log(chalk.blue('Hello world!'));
+const chalk = require('./node_modules/chalk');
+// Use filesystem package
+const fs = require('fs');
+// Use modularized functions
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// This probably won't change, but let's take it out of the procedural code just in case.
+const fsFileName = 'README.md';
+
+// Array of questions for user input via Inquirer
+const questions = [
+    {
+        type: 'input',
+        message: 'Enter the title of your project',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Enter the project description',
+        name: 'description'
+    },
+    {
+        type: 'list',
+        message: question3,
+        name: 'comm_pref'
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log(chalk.white.bgGreen("README file generated successfully."))
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((answer) => {
+        try {
+            const data = generateMarkdown(answer);
+        } catch(err) {
+            console.error("Error generating markdown: ", err);
+        }
+
+    });
+}
 
 // Function call to initialize app
 init();
