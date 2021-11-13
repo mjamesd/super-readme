54134816dd8cb0eee035fdc3a768880d62cc48be
Mark Drummond <mjamesd@gmail.com>
2021-11-12 22:40:34 -0800

    added inquirer. added chalk. added mock readmes.

--- index.js
+++ index.js
@@ -7,26 +7,63 @@ const chalk = require('./node_modules/chalk');
 const fs = require('fs');
 // Use modularized functions
 const generateMarkdown = require('./utils/generateMarkdown');
+const { type } = require('os');
 
-// This probably won't change, but let's take it out of the procedural code just in case.
-const fsFileName = 'README.md';
+// Change these for development
+const fsFileName = 'output.md';
+const fsFolder = "."; // do not include trailing slash
 
 // Array of questions for user input via Inquirer
 const questions = [
     {
         type: 'input',
-        message: 'Enter the title of your project',
-        name: 'title'
+        name: 'title',
+        message: 'Enter the title of your project:'
     },
     {
         type: 'input',
-        message: 'Enter the project description',
-        name: 'description'
+        name: 'description',
+        message: 'Enter the description of your project:'
     },
     {
-        type: 'list',
-        message: question3,
-        name: 'comm_pref'
+        type: 'input',
+        name: 'features',
+        message: 'Enter your project\'s features:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter installation instructions for your project:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage instructions of your project:'
+    },
+    {
+        type: 'input',
+        name: 'usageImg',
+        message: 'Enter the URL or relative filepath of an image for how to use your project:'
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'List your collaborators, if any, with links to their GitHub profiles:'
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'Choose a license for your project:'
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'How people can contribute to your project:'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide examples of how to run tests for your project:'
     }
 ];
 
@@ -37,18 +74,33 @@ function writeToFile(fileName, data) {
     );
 }
 
-// TODO: Create a function to initialize app
-function init() {
+// Initialization fuction
+// User can pass filename to use
+function init(userFileName) {
+    // Check if user supplied a filename
+    let thisFilePath = fsFolder + path.sep + fsFileName;
+    if (userFileName !== null && typeof userFileName === "string") {
+        userFileExtension = userFileName.split('.').pop();
+        if (userFileExtension === "md") {
+            thisFilePath = fsFolder + path.sep + userFileName + userFileExtension;
+        } else {
+            return new Error(`File extension must be ".md".`);
+        }
+    }
     inquirer.prompt(questions)
     .then((answer) => {
         try {
             const data = generateMarkdown(answer);
         } catch(err) {
-            console.error("Error generating markdown: ", err);
+            console.error(chalk.black.bgRed("Error generating markdown: ", err));
         }
-
+        try {
+            writeToFile(thisFilePath, data); // <-- use thisFilePath here <--
+        } catch (err) {
+            console.error(chalk.black.bgRed("Error writing file: ", err));
+        }
     });
 }
 
 // Function call to initialize app
-init();
+init(process.argv[1]);
---
+++ output.md
@@ -0,0 +1,40 @@
+# data.title
+  ![status: in development](https://img.shields.io/badge/status-in%20development-orange)
+
+  data.description
+
+## Table of Contents
+- [Features](#features)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Tests](#tests)
+- [Credits](#credits)
+- [License](#license)
+- [How to Contribute](#how-to-contribute)
+
+
+## Features
+Here are the features:
+ - adsf
+ - qwer
+ - zxcv
+
+## Installation
+First step is that you install it.
+
+## Usage
+This is it.
+
+![Usage demo](data.usageImg)
+
+## Tests
+did you?
+
+## Credits
+me
+
+## License
+gnu3
+
+## How to Contribute
+no
